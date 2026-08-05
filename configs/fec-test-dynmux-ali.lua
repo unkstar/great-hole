80176126b9833964689204211468b4396369f661
unkstar <unkstar@gmail.com>
2026-08-06 00:32:05 +0800

    configs: dyn_mux test configs for RS FEC (port 20086, PSK 0123456789abcdef)

---
+++ configs/fec-test-dynmux-ali.lua
@@ -0,0 +1,42 @@
+-- Ali side: TEST-ONLY RS FEC tunnel via dyn_mux (PSK negotiation)
+local test_port = 20086
+
+m = hole.udp_dyn_mux(test_port)
+c = m:create_channel("0123456789abcdef")
+t = hole.tun("fec-test")
+
+local fec_cfg = {
+    fec_codec = "rs",
+    timeout_ms = 1,
+    overhead = 0.01,
+    max_overhead = 0.50,
+    repeat_ratio = 4.0,
+    symbol_size = 1440,
+    mtu = 1420,
+    max_batch = 20,
+    obfuscate = false,
+    iv_len = 4,
+    decode_window = 64,
+    ping_interval_ms = 1000,
+    feedback_timeout_ms = 2000,
+    feedback_stale_ms = 10000,
+    ping_loss_threshold = 5,
+    decode_timeout_ms = 200,
+    algo = 3,
+    safety_margin = 0.01,
+    loss_window_groups = 50,
+    loss_alpha = 0.1,
+}
+
+local shared = hole.fec_shared_state()
+p_enc = hole.fec_pipeline(t, {}, c, fec_cfg, true, shared)
+p_dec = hole.fec_pipeline(c, {}, t, fec_cfg, false, shared)
+
+print("Ali RS FEC-test dyn_mux started, port " .. test_port)
+
+hole.wait_for_exit()
+p_dec:stop()
+p_enc:stop()
+t:stop()
+c:stop()
+m:stop()
