90639c42aeb4f6c67b339b2f491c494fa0a2d7cb
unkstar <unkstar@gmail.com>
2026-08-05 20:05:16 +0800

    fix: use AND-joined INSTALL_COMMAND in lcrq ExternalProject (repeated args were merged)

--- cmake/lcrq.cmake
+++ cmake/lcrq.cmake
@@ -25,7 +25,7 @@ ExternalProject_Add(lcrq_ext
     CONFIGURE_COMMAND ${LCRQ_SRC}/configure
     BUILD_COMMAND ${CMAKE_MAKE_PROGRAM} -C src
     INSTALL_COMMAND ${CMAKE_COMMAND} -E make_directory ${LCRQ_INSTALL_DIR}/lib
-    INSTALL_COMMAND ${CMAKE_AR} rcs ${LCRQ_INSTALL_DIR}/lib/liblcrq.a src/*.o
+                    AND ${CMAKE_AR} rcs ${LCRQ_INSTALL_DIR}/lib/liblcrq.a src/*.o
     BUILD_BYPRODUCTS ${LCRQ_INSTALL_DIR}/lib/liblcrq.a
 )
 
